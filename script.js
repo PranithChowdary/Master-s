8bfdd9367ba0b5c6e6a7ed9777251effe0514351
Pranith chowdary Karumanchi <pranithtpm@gmail.com>
2023-01-13 15:56:58 +0000

    new

--- script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript
-const toggleBtn = document.querySelector('#drawer-toggle');
-const drawer = document.querySelector('#sidebar-drawer');
-
-toggleBtn.addEventListener('click', () => {
-  drawer.classList.toggle('open');
-});
